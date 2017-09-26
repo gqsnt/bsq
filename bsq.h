2cdd3eef1d7c9026eb2fe9d9caa675c530758003
gquesnot <32057757+gquesnot@users.noreply.github.com>
2017-09-26 12:24:49 +0200

    fez
    fez

---
+++ bsq.h
@@ -0,0 +1,19 @@
+#ifndef BSQ_H
+# define BSQ_H
+
+
+
+typedef struct s_coor
+{
+    int x;
+    int y;
+    int size;
+}   t_coor;
+
+typedef struct s_char
+{
+    char good;
+    char block;
+    char wildcard;
+}   t_char;
+#endif
